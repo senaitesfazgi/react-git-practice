b7b1e61f4205d964252396d5485b419333126903
senaitesfazgi <61923461+senaitesfazgi@users.noreply.github.com>
2020-07-30 11:04:35 -0600

    Added new services component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import About from './components/About';
+import Services from './components/Services';
 
 import './App.css';
 
@@ -9,6 +10,7 @@ function App() {
       <h1>Git Branching / Merge Practice</h1>
       <p>Let's practice git branching and merging</p>
       <About/>,
+      <Services/>
     </div>
 
   );
