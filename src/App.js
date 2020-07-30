10702f94b127ced5a98caa4922601dc6d15708b8
senaitesfazgi <61923461+senaitesfazgi@users.noreply.github.com>
2020-07-30 11:37:13 -0600

    Added contact component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import About from './components/About';
 import Services from './components/Services';
+import Contact from './components/Contact';
 
 import './App.css';
 
@@ -8,9 +9,10 @@ function App() {
   return (
     <div className="App">
       <h1>Git Branching / Merge Practice</h1>
-      <p>Let's practice some git branching and merging</p>
+      <p>Let's practice git branching and merging</p>
       <About/>,
-      <Services/>
+      <Services/>,
+      <Contact/>
     </div>
 
   );
---
+++ src/components/Contact.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class Contact extends React.Component
+{
+    render()
+    {
+        return(
+            <>
+                <h2>Contact Us</h2>
+                <p>Please reachout to work with or if you have any 
+                    questions let know
+                </p>
+                <dl>
+                    <dt>Phone</dt>
+                    <dd>+1 780 717 3216</dd>
+                    <dd>hello@example.com</dd>
+                </dl>
+            </>
+        )
+    }
+}
+
+export default Contact;
